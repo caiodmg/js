4f79a415a72eb7d1fbeb62f3c262e9fd064b0e5a
CAIO DOMINGOS <115330991+caiodmg@users.noreply.github.com>
2023-02-04 00:50:14 -0300

    Add files via upload

---
+++ type-boolean.js
@@ -0,0 +1,15 @@
+// tipo boolean
+// true -> verdadeiro
+// false -> falso
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+const cadastroAtivado = true;
+
+//console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "alura";
+const texto2 = "Alura";
+
+console.log(texto1 === texto2);
+
---
+++ type-strings.js
@@ -0,0 +1,18 @@
+// tipo Strings
+
+const texto1 = "Olá, Mundo!";
+const texto2 = 'Olá, Mundo!';
+const senha = "senhaSuperSegura456";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é'";
+const meuNome = "Leonardo";
+
+// concatenação (+)
+
+console.log(citacao + meuNome);
+
+
+
+//template string ou template literal 
+
