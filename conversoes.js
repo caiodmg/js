7b94bdeda5f7fc727bee0a5c778205271d1c2ec8
CAIO DOMINGOS <115330991+caiodmg@users.noreply.github.com>
2023-02-06 01:24:42 -0300

    Add files via upload

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+// tipo de dado 
+// boleanos 
+
+// conversao implicita 
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero + numeroString);
+
+// conversao explicita 
+
+const numeroString = "456";
+const numeroString = Nummber("456")
